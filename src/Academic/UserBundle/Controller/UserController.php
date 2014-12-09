dd54d1fbdbc0bf7e7c382e623b4072331a8a6edd
Andrey Agafonkin <aagafonkin@magecore.com>
2014-12-09 12:01:04 +0200

    Activities, Collaborators, Main page, User page, Project Page

--- src/Academic/UserBundle/Controller/UserController.php
+++ src/Academic/UserBundle/Controller/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 {
     /**
      * @Route("/userlist", name="user_list")
-     * @Template("AcademicUserBundle:user:userlist.html.twig")
+     * @Template("AcademicUserBundle:User:userlist.html.twig")
      */
     public function userlistAction(Request $request)
     {
@@ -92,7 +92,7 @@ class UserController extends Controller
 
     /**
      * @Route("/userprofile", name="user_profile")
-     * @Template("AcademicUserBundle:user:userlist.html.twig")
+     * @Template("AcademicUserBundle:User:userlist.html.twig")
      */
     public function userprofileAction(Request $request)
     {
