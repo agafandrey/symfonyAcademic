ebb7f0674cc6e03fde4a24f839fdca6908fed590
Andrey Agafonkin <aagafonkin@magecore.com>
2014-12-09 18:27:25 +0200

    Acme Removed. UserBundle IndexController functional test

---
+++ src/Academic/UserBundle/Tests/Controller/IndexControllerTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Academic\UserBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class IndexControllerTest extends WebTestCase
+{
+    private $client;
+
+    protected function setUp()
+    {
+        $this->client = static::createClient(
+            array(), array(
+                'PHP_AUTH_USER' => 'admin',
+                'PHP_AUTH_PW'   => 'admin',
+            )
+        );
+    }
+
+    public function testIndex()
+    {
+        $url = $this->client->getContainer()->get('router')->generate('index', array(), false);
+        $this->client->request('GET', $url);
+        $result = $this->client->getResponse();
+        //var_dump($result->getStatusCode());die;
+        $this->assertEquals(
+            200,
+            $result->getStatusCode()
+        );
+    }
+
+    public function testLogin()
+    {
+        $client = static::createClient();
+        $url = $client->getContainer()->get('router')->generate('login', array(), false);
+
+        $crawler = $this->client->request('GET', $url);
+        $this->client->followRedirects(true);
+        $form = $crawler->selectButton('login')->form();
+        $form['_username'] = 'admin';
+        $form['_password'] = 'admin';
+        $this->client->submit($form);
+
+        $result = $this->client->getResponse();
+        $this->assertEquals(
+            200,
+            $result->getStatusCode()
+        );
+    }
+
+    public function testLogout()
+    {
+        $url = $this->client->getContainer()->get('router')->generate('index', array(), false);
+        $crawler = $this->client->request('GET', $url);
+        $this->client->followRedirects(true);
+        $link = $crawler->filter('a:contains("Log out")')->link();
+        $this->client->click($link);
+        $this->assertEquals(
+            200,
+            $this->client->getResponse()->getStatusCode()
+        );
+    }
+}
